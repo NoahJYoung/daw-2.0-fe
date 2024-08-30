e93968024b7df6abae436e1eb3c50d221419c25e
Noah Young <noah.young@sqa-consulting.com>
2024-08-30 20:11:59 +0200

    knob rotation angle

--- src/components/ui/custom/knob.tsx
+++ src/components/ui/custom/knob.tsx
@@ -8,13 +8,34 @@ interface KnobProps {
 }
 
 export const Knob = ({ value, onChange, min, max, step, size }: KnobProps) => {
+  const initialRotation = 135;
+  // Define the rotation range
+  const minRotation = -135;
+  const maxRotation = 135;
+
+  // Calculate the rotation based on the value
+  const rotation =
+    minRotation +
+    ((value - min) / (max - min)) * (maxRotation - minRotation) +
+    initialRotation;
   return (
-    <div style={{ width: size, height: size }}>
-      <svg>
-        {/* <circle width={size} height={size} fill="black" /> */}
-        <circle cx={size} cy={size} r={size} />
-        <circle cx={size * 0.2} cy={size} r={size * 0.2} fill="blue" />
-      </svg>
-    </div>
+    <svg
+      width={size}
+      height={size}
+      style={{
+        transform: `rotate(${rotation}deg)`,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <circle
+        cx={size - size / 2}
+        cy={size - size / 2}
+        r={size / 2}
+        fill="#333"
+      />
+      <circle cx={size * 0.1 + 3} cy={size * 0.75} r={size * 0.1} fill="#999" />
+    </svg>
   );
 };
--- src/pages/studio/components/top-panel/components/track-panels/components/track-panel/track-panel.tsx
+++ src/pages/studio/components/top-panel/components/track-panels/components/track-panel/track-panel.tsx
@@ -2,6 +2,7 @@ import { observer } from "mobx-react-lite";
 import { Track } from "@/pages/studio/audio-engine/components";
 import { BASE_TRACK_HEIGHT } from "@/pages/studio/utils/constants";
 import { Knob } from "@/components/ui/custom/knob";
+import { useState } from "react";
 
 interface TrackPanelProps {
   track: Track;
@@ -9,28 +10,31 @@ interface TrackPanelProps {
 }
 
 export const TrackPanel = observer(
-  ({ track, trackNumber }: TrackPanelProps) => (
-    <div
-      className="flex bg-surface-1 border border-surface-0 border-b-0"
-      style={{
-        width: "100%",
-        flexShrink: 0,
-        height: BASE_TRACK_HEIGHT,
-        gap: "1rem",
-      }}
-    >
-      <div className="h-full p-1 w-8 flex items-center justify-center bg-surface-2 text-surface-4 font-bold border border-surface-1 border-b-0">
-        {trackNumber}
+  ({ track, trackNumber }: TrackPanelProps) => {
+    const [knobValue, setKnobValue] = useState(50);
+    return (
+      <div
+        className="flex bg-surface-1 border border-surface-0 border-b-0"
+        style={{
+          width: "100%",
+          flexShrink: 0,
+          height: BASE_TRACK_HEIGHT,
+          gap: "1rem",
+        }}
+      >
+        <div className="h-full p-1 w-8 flex items-center justify-center bg-surface-2 text-surface-4 font-bold border border-surface-1 border-b-0">
+          {trackNumber}
+        </div>
+        <span className="text-surface-7">{track.name}</span>
+        <Knob
+          size={24}
+          value={knobValue}
+          max={100}
+          min={0}
+          step={1}
+          onChange={() => {}}
+        />
       </div>
-      <span className="text-surface-7">{track.name}</span>
-      <Knob
-        size={16}
-        value={0}
-        max={100}
-        min={0}
-        step={1}
-        onChange={() => {}}
-      />
-    </div>
-  )
+    );
+  }
 );
