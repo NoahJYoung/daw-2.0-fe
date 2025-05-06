76d76db47ea18751f749e19c65fbfe9e4b138a1f
Noah Young <noah.young@sqa-consulting.com>
2025-05-06 22:50:52 +0200

    added new project button to mobiel view

--- src/pages/dashboard/projects-dashboard/components/empty-projects/empty-projects.tsx
+++ src/pages/dashboard/projects-dashboard/components/empty-projects/empty-projects.tsx
@@ -12,7 +12,7 @@ export const EmptyProjects = () => {
         You haven't created any audio projects yet. Start creating or import an
         existing project.
       </p>
-      <div className="hidden gap-2 lg:flex">
+      <div className="gap-2 flex">
         <button
           onClick={() => alert("not implemented yet")}
           className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
